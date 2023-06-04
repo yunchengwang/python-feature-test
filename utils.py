da0211ea438aa68e092f35fafb26a12301b4db07
yunchengwang <joewang622@gmail.com>
2023-06-04 16:57:46 -0700

    update

--- utils.py
+++ utils.py
@@ -5,13 +5,20 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 
 class FeatureTest:
     def __init__(self, loss='bce'):
-        assert loss in ['bce', 'ce', 'mse'], f'loss not defined. Please select from ["bce", "ce", "mse"]'
+        assert loss in ['bce', 'ce', 'mse'], f'loss not defined. Please select from ["bce", "ce", "mse"].'
         self.loss = loss
+        self.dim_loss = dict()
         self.sorted_features = None
         self.dim = 0
 
     def fit(self, X, y, n_bins, outliers=False):
-        pass
+        self.dim = X.shape[1]
+        for d in range(self.dim):
+            min_partition_loss = self.get_min_partition_loss(X[:, d], y, n_bins, outliers)
+            self.dim_loss[d] = min_partition_loss
+
+        self.dim_loss = {k: v for k, v in sorted(self.dim_loss.items(), key=lambda item: item[1])}
+        self.sorted_features = np.array(list(self.dim_loss.keys()))
 
     def transform(self, X, n_selected):
         assert self.sorted_features, f'Run fit() before selecting features.'
@@ -31,21 +38,36 @@ class FeatureTest:
         for i in range(1, n_bins):
             partition_point = f_min + i * bin_width
             y_l, y_r = y[f_1d <= partition_point], y[f_1d > partition_point]
-            partition_error = self.get_loss(y_l, y_r, self.loss)
-            if partition_error < best_error:
-                best_error = partition_error
-                best_partition_index = i
-                left_mean = left_mos.mean()
-                right_mean = right_mos.mean()
-        return best_error, best_partition_index, left_mean, right_mean
+            partition_loss = self.get_loss(y_l, y_r)
+            if partition_loss < min_partition_loss:
+                min_partition_loss = partition_loss
+        return min_partition_loss
 
-    @staticmethod
-    def get_loss(y_l, y_r, loss):
-        if loss == 'bce':
+    def get_loss(self, y_l, y_r):
+        n1, n2 = len(y_l), len(y_r)
+        if self.loss == 'bce':
+            lp = y_l.mean()
+            if lp == 1 or lp == 0:
+                lh = 0.0
+            else:
+                lh = np.sum(-y_l * np.log2(lp) - (1 - y_l) * np.log2(1 - lp))
+            rp = y_r.mean()
+            if rp == 1 or rp == 0:
+                rh = 0.0
+            else:
+                rh = np.sum(-y_r * np.log2(rp) - (1 - y_r) * np.log2(1 - rp))
+            return (lh + rh) / (n1 + n2)
+        elif self.loss == 'ce':
             pass
-        elif loss == 'ce':
-            pass
-        elif loss == 'mse':
-            pass
+        elif self.loss == 'mse':
+            left_mse = ((y_l - y_l.mean()) ** 2).sum()
+            right_mse = ((y_r - y_r.mean()) ** 2).sum()
+            return np.sqrt((left_mse + right_mse) / (n1 + n2))
         else:
             pass
+
+    @staticmethod
+    def remove_outliers(f_1d, y, n_std=2.0):
+        """Remove outliers for the regression problem."""
+        f_mean, f_std = f_1d.mean(), f_1d.std()
+        return f_1d[np.abs(f_1d - f_mean) <= n_std * f_std], y[np.abs(f_1d - f_mean) <= n_std * f_std]
